44a0534d7c70f1fa945d2d803b082f3b4822b8c9
Nhan Phan <nphanacc@gmail.com>
2021-07-29 16:17:16 -0700

    add function to split deck in 2 equal pile, clean up computerCardSlot, playerCardSlot , winner defined text, and function to update the deck count of computer and player

--- script.js
+++ script.js
@@ -2,10 +2,46 @@
 import Deck from "./deck.js";
 
 //Access to computer-card-slot div
-const computerCardSlot = document.querySelector('.computer-card-slot');
+const computerCardSlot = document.querySelector(".computer-card-slot");
+const playerCardSlot = document.querySelector(".player-card-slot");
+const computerDeckElement = document.querySelector(".computer-deck");
+const playerDeckElement = document.querySelector(".player-deck");
+const text = document.querySelector(".text");
 
-const deck = new Deck();
-deck.shuffle();
-console.log(deck.cards);
+let playerDeck, computerDeck;
 
-computerCardSlot.appendChild(deck.cards[0].getHTML());
+startGame();
+
+function startGame() {
+	const deck = new Deck();
+	deck.shuffle();
+
+	// Split deck into 2 equal pile of cards
+	const deckMidpoint = Math.ceil(deck.numberOfCards / 2);
+	playerDeck = new Deck(deck.cards.slice(0, deckMidpoint));
+	computerDeck = new Deck(deck.cards.slice(deckMidpoint, deck.numberOfCards));
+
+	console.log(playerDeck);
+	console.log(computerDeck);
+
+	cleanBeforeRound();
+}
+
+// Clean up function before round
+function cleanBeforeRound() {
+	computerCardSlot.innerHTML = "";
+	playerCardSlot.innerHTML = "";
+	text.innerText = "";
+
+	updateDeckCount();
+}
+
+//update deck count to run inside cleanBeforeRound
+function updateDeckCount() {
+	computerDeckElement.innerText = computerDeck.numberOfCards;
+	playerDeckElement.innerText = playerDeck.numberOfCards;
+}
+
+// console.log(deck.cards);
+
+// computerCardSlot.appendChild(deck.cards[0].getHTML());
